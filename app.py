82ea1753cd9166f148f16dc168c461a17bb60160
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-08-12 12:39:05 -0300

    add SQLAlchemy  and create person model

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import render_template
-import connexion
+
+import config
+
+from models import Person
 
 
-app = connexion.App(__name__, specification_dir="./")
-app.add_api("swagger.yml")
+app = config.connex_app
+app.add_api(config.basedir / "swagger.yml")
 
 
 @app.route("/")
