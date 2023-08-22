d8b08d05fc6909f8d89bd14f582c790cc293e770
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-08-22 17:04:01 -0300

    vue pegando pessoa via API

--- static/vue/components/PersonContent.js
+++ static/vue/components/PersonContent.js
@@ -1,25 +1,20 @@
 app.component('person', {
-    props: {
-    },
+    props: [
+      'person'
+    ],
     template:
       /*html*/
       `
       <div class="person-card" data-person-id="{{ person.id }}">
         <div class="person-content">
         <h2>
-          <span data-person-fname="{{ person.fname }}">{{ person.fname }}</span>
-          <span data-person-lname="{{ person.lname }}">{{ person.lname }}</span>
+        <span data-person-fname="{{ person.fname }}">{{ person.fname }}</span>
+        <span data-person-lname="{{ person.lname }}">{{ person.lname }}</span>
         </h2>
         </div>
       </div>
       `,
     data() {
-      return {
-        person: {
-            'fname': "Anselmo",
-            'lname': "BD",
-        },
-      }
     },
     methods: {
     },
--- static/vue/main.js
+++ static/vue/main.js
@@ -1,5 +1,16 @@
 const app = Vue.createApp({
     data() {
-        return {}
+        return {
+            person: {}
+        }
+    },
+    mounted() {
+        axios.get('http://localhost:5000/api/people/1')
+        .then(response => {
+            this.person = response.data;
+        })
+        .catch(error => {
+            console.error('Erro ao obter person via API:', error);
+        });
     }
 })
