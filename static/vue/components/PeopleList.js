e78614b1ba100649fbcddcbee8d45cd7fa74f8b2
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-08-23 15:31:40 -0300

    criado componente person-form para criar novas persons

--- static/vue/components/PeopleList.js
+++ static/vue/components/PeopleList.js
@@ -5,6 +5,9 @@ app.component('people-list', {
   template:
     /*html*/
     `
+    <div class="person-create-card">
+      <person-form :people="people"></person-form>
+    </div>
     <div class="people-list">
       <person-card v-for="person in people" :person="person"></person-card>
     </div>
---
+++ static/vue/components/PersonForm.js
@@ -0,0 +1,56 @@
+app.component('person-form', {
+  props: [
+    'people'
+  ],
+  template:
+    /*html*/
+    `
+    <form @submit.prevent="onSubmit">
+    <label for="fname"><span>First Name</span><span class="input-error">{{ error.fname }}</span></label>
+    <input v-model="fname" id="fname" type="text"/>
+    <label for="lname"><span>Last Name</span><span class="input-error">{{ error.lname }}</span></label>
+    <input v-model="lname" id="lname" type="text"/>
+    <button type="submit">✨ Create Person</button>
+    </form>
+    `,
+  data() {
+    return {
+      fname: '',
+      lname: '',
+      error: {
+        'fname': '',
+        'lname': '',
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      for(const key in this.error)
+        this.error[key] = "";
+      let newPerson = {
+        fname: this.fname,
+        lname: this.lname
+      }
+      axios.post('http://localhost:5000/api/people', newPerson)
+      .then(response => {
+          person = response.data;
+          this.people.push(person);
+      })
+      .catch(error => {
+        e_data = error.response.data
+        if (e_data.status == 400) {
+          detail = JSON.parse(e_data.detail);
+          for (const [key, value] of Object.entries(detail)) {
+            this.error[key] = value.join('; ');
+          }
+        } else {
+          console.error('Error persisting person via API:', error);
+        }
+      });
+      this.fname = ''
+      this.lname = ''
+    }
+  },
+  computed: {
+  }
+})
