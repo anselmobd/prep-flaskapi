8909fdaab81998ac2395d436b381ba31766aa289
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-08-27 21:23:36 -0300

    person Delete não estava funcionando no vuemod

--- static/vuemod/main.js
+++ static/vuemod/main.js
@@ -11,7 +11,9 @@ export default {
     <h1>Pessoas e mensagens</h1>
     <people-list
       :people="people"
-    ></people-list>
+      @person-update="personUpdate"
+      @person-delete="personDelete"
+  />
     `,
   data() {
     return {
@@ -29,9 +31,13 @@ export default {
   },
   methods: {
     personUpdate(value) {
-      person = this.people.find(a => a.id === value.id)
-      for(const key in value)
+      let person = this.people.find(a => a.id === value.id)
+      for (const key in value)
         person[key] = value[key];
+    },
+    personDelete(id) {
+      let index = this.people.findIndex(a => a.id === id);
+      this.people.splice(index, 1);
     }
   }
 }
