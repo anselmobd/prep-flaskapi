d1e86c6d25ca435725ffaaff0a538e019e9631d3
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-09-09 15:46:03 -0300

    rápida tentativa de utilizar Flask-Alembic

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import pathlib
 import connexion
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-
+from flask_alembic import Alembic
 
 basedir = pathlib.Path(__file__).parent.resolve()
 connex_app = connexion.App(__name__, specification_dir=basedir)
@@ -13,3 +13,4 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+alembic = Alembic()
